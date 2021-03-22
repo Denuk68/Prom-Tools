7ded4d8b9d9ace6bd22c1a3701ae388155b15406
Denuk68 <denus.denisovuch@gmail.com>
2021-03-22 13:29:05 +0200

    Added Scroll Js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Header
 $(document).ready(function(){
     $('.header__burger').click(function(event){
         $('.header__burger, .header__menu').toggleClass('active');
@@ -7,4 +8,19 @@ $(document).ready(function(){
         $('.header__burger, .header__menu').toggleClass('active');
     });
 });
-
+// Scroll
+$(document).ready(function () {
+    var offset = $(".header").innerHeight();
+    $(".header__menu ul li a[href^='#']").click(function () {
+      var target = $(this).attr("href");
+      $("html, body").animate(
+        {
+          scrollTop: $(target).offset().top - offset,
+        },
+        500
+      );
+      $(".header__menu ul li a[href^='#']").removeClass("active");
+      $(this).addClass("active");
+      return false;
+    });
+  });
